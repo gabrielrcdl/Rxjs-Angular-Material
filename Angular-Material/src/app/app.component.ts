e5c4a714179cb945c36a4f11c2e05a6b0b5e799b
Gabriel Cardoso <gr78787@gmail.com>
2022-11-22 22:16:24 -0300

    toolbar

--- Angular-Material/src/app/app.component.ts
+++ Angular-Material/src/app/app.component.ts
@@ -1,18 +1,44 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { MatSidenav } from '@angular/material/sidenav';
+import { fromEvent, map } from 'rxjs';
+
+export const SCROLL_CONTAINER = 'mat-sidenav-content';
+export const TEXT_LIMIT = 50;
+export const SHADOW_LIMIT = 100;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public isSmallScreen: boolean = false;
+  public showText: boolean = false;
+  public  applyShadow: boolean = false;
 
 
 
   constructor(private breakpointObserver: BreakpointObserver) {}
+
+
+  ngOnInit(): void {
+    const container = document.getElementsByClassName(SCROLL_CONTAINER)[0];
+
+
+    fromEvent(container, 'scroll')
+    .pipe(
+      map(() => container.scrollTop)
+    )
+    .subscribe({
+      next: (value: number) => this.determineHeader(value)// next recebe o resultado do map
+    })
+  }
+
+  determineHeader( scrollTop: number){
+    this.showText = scrollTop >=  TEXT_LIMIT;
+    this.applyShadow = scrollTop >= SHADOW_LIMIT
+  }
 
   ngAfterContentInit(): void {
     // Observando o tempo todo o tamanho da tela
